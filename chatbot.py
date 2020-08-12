823a94f05bcb0c50a3c96998cfb4fca14be96426
Rishabh Dwivedi <57454462+Rishabh062@users.noreply.github.com>
2020-08-12 21:22:55 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,33 @@
+#python 3.7.1
+#simple AI chatbot
+import time 
+print("Hello,hope you are good")
+time.sleep(1)
+print("my name is roboxp")
+time.sleep(0.5)
+name=input("what is your name?")
+time.sleep(1)
+print("oooo"+" "+name+" "+"nice name")
+doi=input("What do you do"+" "+name)
+print("Thats nice!\n"+" "+"so you are doing"+""+doi)
+check=input("Are you enjoying :-) press y for yes and n for not")
+if(check=='y'):
+  print("Thats awesome")
+else:
+  print("sad to hear that"+" "+name)
+print("ok")
+hero=input("who is your favourite hero")
+print("i also like that",hero)
+ask=input("do you have any questions y for yes and n for not")
+if(ask=='y'):
+  tell=input("Tell me about that")
+  print("Sad to hear that,but concentrate on that it will be solved, have hope")
+else:
+  print("okk this was nice meeting ")
+time.sleep(1)
+print("byee")
+print("ok meet u again")
+
+print("______meeting is ended_______")
+print("created by Rishabh Dwivedi")
+
